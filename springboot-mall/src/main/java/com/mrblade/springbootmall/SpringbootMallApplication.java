28b486f02f2f2d1414567d24f98461053d05ad53
user <mr.blade520@gmail.com>
2026-03-02 23:34:00 +0800

    初始化 springboot mall

---
+++ springboot-mall/src/main/java/com/mrblade/springbootmall/SpringbootMallApplication.java
@@ -0,0 +1,13 @@
+package com.mrblade.springbootmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMallApplication.class, args);
+    }
+
+}
